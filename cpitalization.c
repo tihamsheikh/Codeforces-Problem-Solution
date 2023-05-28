c00da72e9c36be425741e52d5abd4d73a29109de
TihamX <105205393+Tiham69@users.noreply.github.com>
2023-05-28 22:38:58 +0600

    Add files via upload

---
+++ cpitalization.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char s[1000];
+
+    scanf("%s", &s);
+
+    if(s[0] >= 'a' && s[0] <= 'z')
+    {
+        s[0] = s[0] - 32;
+    }
+
+    printf("%s\n", s);
+}
