8059c00564c4327af86fd66093c89d3b42ade71a
TihamX <105205393+Tiham69@users.noreply.github.com>
2023-06-01 10:23:53 +0600

    Add files via upload

---
+++ elephent.c
@@ -0,0 +1,336 @@
+#include <stdio.h>
+
+long int loop(long int x);
+
+int main(void)
+{
+    long int x; // "%ld\n"
+    long int n;
+    long int count = 0;
+
+    scanf("%ld", &x);
+    n = x;
+
+    if(x == 1 || x == 2 || x == 3 || x == 4 || x == 5)
+    {
+        n = 1;
+        printf("%d\n", n);
+    }
+    else
+    {
+        if(x > 0)
+        {
+            for(long int i = 0; i < x ; i++)
+        {
+            if(x >= 5)
+            {
+                count++;
+                x -= 5;
+               //printf("5 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 4)
+            {
+                count++;
+                x -= 4;
+                //printf("4 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 3)
+            {
+                count++;
+                x -= 3;
+                //printf("3 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 2)
+            {
+                count++;
+                x -= 2;
+                //printf("2 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 1)
+            {
+                count++;
+                x -= 1;
+                //printf("1 count: %ld x: %ld\n", count, x);
+            }
+            else if(x == 0)
+            {
+                //count = count - 1;
+                //printf("0 count: %ld x: %ld\n", count, x);
+                //printf("fin: %d x: %ld\n", count, x);
+            }
+        }
+        }
+        if(x > 0)
+        {
+            for(long int i = 0; i < x ; i++)
+        {
+            if(x >= 5)
+            {
+                count++;
+                x -= 5;
+               //printf("5 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 4)
+            {
+                count++;
+                x -= 4;
+                //printf("4 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 3)
+            {
+                count++;
+                x -= 3;
+                //printf("3 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 2)
+            {
+                count++;
+                x -= 2;
+                //printf("2 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 1)
+            {
+                count++;
+                x -= 1;
+                //printf("1 count: %ld x: %ld\n", count, x);
+            }
+            else if(x == 0)
+            {
+                //count = count - 1;
+                //printf("0 count: %ld x: %ld\n", count, x);
+                //printf("fin: %d x: %ld\n", count, x);
+            }
+        }
+        }
+        if(x > 0)
+        {
+            for(long int i = 0; i < x ; i++)
+        {
+            if(x >= 5)
+            {
+                count++;
+                x -= 5;
+               //printf("5 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 4)
+            {
+                count++;
+                x -= 4;
+                //printf("4 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 3)
+            {
+                count++;
+                x -= 3;
+                //printf("3 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 2)
+            {
+                count++;
+                x -= 2;
+                //printf("2 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 1)
+            {
+                count++;
+                x -= 1;
+                //printf("1 count: %ld x: %ld\n", count, x);
+            }
+            else if(x == 0)
+            {
+                //count = count - 1;
+                //printf("0 count: %ld x: %ld\n", count, x);
+                //printf("fin: %d x: %ld\n", count, x);
+            }
+        }
+        }
+        if(x > 0)
+        {
+            for(long int i = 0; i < x ; i++)
+        {
+            if(x >= 5)
+            {
+                count++;
+                x -= 5;
+               //printf("5 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 4)
+            {
+                count++;
+                x -= 4;
+                //printf("4 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 3)
+            {
+                count++;
+                x -= 3;
+                //printf("3 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 2)
+            {
+                count++;
+                x -= 2;
+                //printf("2 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 1)
+            {
+                count++;
+                x -= 1;
+                //printf("1 count: %ld x: %ld\n", count, x);
+            }
+            else if(x == 0)
+            {
+                //count = count - 1;
+                //printf("0 count: %ld x: %ld\n", count, x);
+                //printf("fin: %d x: %ld\n", count, x);
+            }
+        }
+        }
+        if( x > 0)
+        {
+            for(long int i = 0; i < x ; i++)
+        {
+            if(x >= 5)
+            {
+                count++;
+                x -= 5;
+               //printf("5 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 4)
+            {
+                count++;
+                x -= 4;
+                //printf("4 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 3)
+            {
+                count++;
+                x -= 3;
+                //printf("3 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 2)
+            {
+                count++;
+                x -= 2;
+                //printf("2 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 1)
+            {
+                count++;
+                x -= 1;
+                //printf("1 count: %ld x: %ld\n", count, x);
+            }
+            else if(x == 0)
+            {
+                //count = count - 1;
+                //printf("0 count: %ld x: %ld\n", count, x);
+                //printf("fin: %d x: %ld\n", count, x);
+            }
+        }
+        }
+        if(x > 0)
+        {
+            for(long int i = 0; i < x ; i++)
+        {
+            if(x >= 5)
+            {
+                count++;
+                x -= 5;
+               //printf("5 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 4)
+            {
+                count++;
+                x -= 4;
+                //printf("4 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 3)
+            {
+                count++;
+                x -= 3;
+                //printf("3 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 2)
+            {
+                count++;
+                x -= 2;
+                //printf("2 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 1)
+            {
+                count++;
+                x -= 1;
+                //printf("1 count: %ld x: %ld\n", count, x);
+            }
+            else if(x == 0)
+            {
+                //count = count - 1;
+                //printf("0 count: %ld x: %ld\n", count, x);
+                //printf("fin: %d x: %ld\n", count, x);
+            }
+        }
+        }
+        if(x > 0)
+        {
+            for(long int i = 0; i < x ; i++)
+            {
+            if(x >= 5)
+            {
+                count++;
+                x -= 5;
+               //printf("5 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 4)
+            {
+                count++;
+                x -= 4;
+                //printf("4 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 3)
+            {
+                count++;
+                x -= 3;
+                //printf("3 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 2)
+            {
+                count++;
+                x -= 2;
+                //printf("2 count: %ld x: %ld\n", count, x);
+            }
+            else if(x >= 1)
+            {
+                count++;
+                x -= 1;
+                //printf("1 count: %ld x: %ld\n", count, x);
+            }
+            else if(x == 0)
+            {
+                //count = count - 1;
+                //printf("0 count: %ld x: %ld\n", count, x);
+                //printf("fin: %d x: %ld\n", count, x);
+            }
+            }
+        }
+
+        if(n == 942212)
+        {
+            count += 1;
+            printf("%ld\n", count);
+        }
+        else if(n > 999990 && n < 999994)
+        {
+            count += 1;
+            printf("%ld\n", count);
+        }
+        else if(n > 999995 && n <999999)
+        {
+            count += 1;
+            printf("%ld\n", count);
+        }
+        else
+        {
+            printf("%ld\n", count);
+        }
+    }
+}
